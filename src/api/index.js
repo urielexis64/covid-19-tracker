a11b90f1fbb90a22ff9b765082989cfc3870e8a5
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-05-23 01:08:05 -0600

    dynamic data passed to Cards

--- src/api/index.js
+++ src/api/index.js
@@ -2,16 +2,24 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+	let changeableUrl = url;
+
+	if (country) {
+		changeableUrl = `${url}/countries/${country}`;
+	}
+
 	try {
 		const {
 			data: {confirmed, recovered, deaths, lastUpdate},
-		} = await axios.get(url);
+		} = await axios.get(changeableUrl);
 
 		const modifiedData = {confirmed, recovered, deaths, lastUpdate};
 
 		return modifiedData;
-	} catch (error) {}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const fetchDailyData = async () => {
@@ -23,5 +31,18 @@ export const fetchDailyData = async () => {
 			date: dailyData.reportDate,
 		}));
 		return modifiedData;
-	} catch (error) {}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const fetchCountries = async () => {
+	try {
+		const {
+			data: {countries},
+		} = await axios.get(`${url}/countries`);
+		return countries.map((country) => country.name);
+	} catch (error) {
+		console.log(error);
+	}
 };
