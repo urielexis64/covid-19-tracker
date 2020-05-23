e2e1b97a28de96c1a73bdf1bf05a4faddcb1b194
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-05-23 02:59:11 -0600

    Updates

---
+++ precache-manifest.1014c4554820ca963749c274f2aebebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625537942d2d499d36bb703babdc3b3e",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "10004af804640b09bd77",
+    "url": "/covid-19-tracker/static/css/main.4aaae70e.chunk.css"
+  },
+  {
+    "revision": "cda0abd618fc589d18a6",
+    "url": "/covid-19-tracker/static/js/2.fcc0dcce.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.fcc0dcce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10004af804640b09bd77",
+    "url": "/covid-19-tracker/static/js/main.2c0853d7.chunk.js"
+  },
+  {
+    "revision": "fe7bce7a7b65f398812b",
+    "url": "/covid-19-tracker/static/js/runtime-main.df93df52.js"
+  }
+]);
